aab275f3a7494d0df6baae5161d6c016adb80511
PFC-webprojects <33673932+PFC-webprojects@users.noreply.github.com>
2018-01-09 18:42:00 -0800

    Week 1 - Tell me about yourself
    Assignment for WEB 150 W18 3237 - INTRO TO JAVASCRIPT

---
+++ week1/script.js
@@ -0,0 +1,14 @@
+/*
+Week 1 - Tell me about yourself
+
+Peter Caliandro
+WEB 150
+Winter 2018
+
+script.js
+*/
+
+var firstName    = "Peter";
+var lastName     = "Caliandro";
+var yearsOfStudy = "two quarters so far";
+var goal         = "to work as a full stack web developer";
